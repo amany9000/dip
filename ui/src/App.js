9e52c152e22426e759230bf047f8278f4c0a5fbc
Rohan2599 <201651044@iiitvadodara.ac.in>
2019-01-19 14:44:57 +0530

    added login page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/login/Login';
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+				<div>
+				<div align="center" className="head1">
+				<h2><b>Welcome !! Create an account</b></h2>
+				</div>
+				<Login />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ ui/src/components/login/Login.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { Row, Form, FormGroup, FormControl, ControlLabel, Button, HelpBlock } from 'react-bootstrap';
+import './login.css';
+import { isEmail, isEmpty, isLength, isContainWhiteSpace } from 'shared/validator';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            formData: {}, // Contains login form data
+            errors: {}, // Contains login field errors
+            formSubmitted: false, // Indicates submit status of login form 
+            loading: false // Indicates in progress state of login form
+        }
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        let { formData } = this.state;
+        formData[name] = value;
+
+        this.setState({
+            formData: formData
+        });
+    }
+
+    validateLoginForm = (e) => {
+        
+        let errors = {};
+        const { formData } = this.state;
+
+        if (isEmpty(formData.email)) {
+            errors.email = "Email can't be blank";
+        } else if (!isEmail(formData.email)) {
+            errors.email = "Please enter a valid email";
+        }
+
+        if (isEmpty(formData.password)) {
+            errors.password = "Password can't be blank";
+        }  else if (isContainWhiteSpace(formData.password)) {
+            errors.password = "Password should not contain white spaces";
+        } else if (!isLength(formData.password, { gte: 6, lte: 16, trim: true })) {
+            errors.password = "Password's length must between 6 to 16";
+        }
+
+        if (isEmpty(errors)) {
+            return true;
+        } else {
+            return errors;
+        }    
+    }
+
+    login = (e) => {
+        
+        e.preventDefault();
+
+        let errors = this.validateLoginForm();
+
+        if(errors === true){
+            alert("You are successfully signed in...");
+            window.location.reload()   
+        } else {
+            this.setState({
+                errors: errors,
+                formSubmitted: true
+            });
+        }
+    }
+
+    render() {
+
+        const { errors, formSubmitted } = this.state;
+
+        return (
+            <div className="Login">
+                <Row>
+                    <form onSubmit={this.login}>
+                        <FormGroup controlId="email" validationState={ formSubmitted ? (errors.email ? 'error' : 'success') : null }>
+                            <ControlLabel>Email</ControlLabel>
+                            <FormControl type="text" name="email" placeholder="Enter your email" onChange={this.handleInputChange} />
+                        { errors.email && 
+                            <HelpBlock>{errors.email}</HelpBlock> 
+                        }
+                        </FormGroup >
+                        <FormGroup controlId="password" validationState={ formSubmitted ? (errors.password ? 'error' : 'success') : null }>
+                            <ControlLabel>Password</ControlLabel>
+                            <FormControl type="password" name="password" placeholder="Enter your password" onChange={this.handleInputChange} />
+                        { errors.password && 
+                            <HelpBlock>{errors.password}</HelpBlock> 
+                        }
+                        </FormGroup>
+                        <Button type="submit" bsStyle="primary">Log-In</Button>
+                    </form>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default Login;
