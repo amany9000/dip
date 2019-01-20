cd1b5a357f0e4eac826211e46018bc87ba7670b9
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-20 06:57:58 +0530

    Added router

--- app/src/App.js
+++ app/src/App.js
@@ -5,32 +5,43 @@ import Typography from '@material-ui/core/Typography';
 import Tabs from './components/tabs';
 import AddIcon from '@material-ui/icons/Add';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import 'antd/dist/antd.css';
 import 'antd/lib/date-picker/style/css';
 import Login from './login.js';
-import'./App.css';
+import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="wrapper">
-          <Typography variant="h3" gutterBottom className="header">
-            Sexy Bitches
-          </Typography>
-          <Tabs />
+      <Router>
+        <div>
+          <Route path='/signin' render={() =>
+            <div className="App">
+              <div >
+                <h2><div align="center" className="main1"><b>BE ORIGINAL!</b></div></h2>
+                <Login />
+              </div>
+            </div>
+          } />
+          <Route path='/search' render={() =>
+            <div className="App">
+              <div className="wrapper">
+                <Typography variant="h3" gutterBottom className="header">
+                  Sexy Bitches
+              </Typography>
+                <Tabs />
+              </div>
+              <IconButton className="mid-icon" aria-label="Menu">
+                <AddIcon style={{ fontSize: 50 }} />
+              </IconButton>
+              <div className="loader">
+                <CircularProgress />
+              </div>
+            </div>
+          } />
         </div>
-        <IconButton className="mid-icon" aria-label="Menu">
-          <AddIcon style={{ fontSize: 50 }}/>
-        </IconButton>
-        <div className="loader">
-          <CircularProgress />
-        </div>
-      <div >
-        <h2><div align="center" className="main1"><b>BE ORIGINAL!</b></div></h2>
-        <Login/>
-      </div>
-      </div>
+      </Router>
     );
   }
 }
